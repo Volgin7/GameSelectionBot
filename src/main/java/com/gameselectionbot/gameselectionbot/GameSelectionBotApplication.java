fc35359d969971a3848f1b0a9413e965d126b1f6
Volgin7 <8612020@gmail.com>
2023-04-12 13:33:14 +0300

    Initial "Hello" bot

---
+++ src/main/java/com/gameselectionbot/gameselectionbot/GameSelectionBotApplication.java
@@ -0,0 +1,14 @@
+package com.gameselectionbot.gameselectionbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSelectionBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameSelectionBotApplication.class, args);
+        System.out.println("Hello world 1");
+    }
+
+}
